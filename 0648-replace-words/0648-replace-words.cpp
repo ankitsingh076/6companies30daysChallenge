77acec68ec5b908d42238c172577b926c07eaeb4
Ankit Kumar Singh <ankitraj.1600405@gmail.com>
2024-06-07 23:01:13 +0530

    Time: 699 ms (13.6%), Space: 481.3 MB (9.88%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,78 +1,34 @@
 class Solution
 {
     public:
-        struct trieNode
+
+        string findRoot(string &word, unordered_set<string> &st)
         {
-            trieNode *children[26];
-            bool isEndOfWord;
-        };
 
-    trieNode* getNode()
-    {
-        trieNode *newNode = new trieNode();
-        newNode->isEndOfWord = false;
-
-        for (int i = 0; i < 26; i++)
-        {
-            newNode->children[i] = NULL;
-        }
-
-        return newNode;
-    }
-
-    trieNode * root;
-
-    /**Inserts a word into the trie. */
-    void insert(string word)
-    {
-        trieNode *crawler = root;
-
-        for (int i = 0; i < word.length(); i++)
-        {
-            int index = word[i] - 'a';
-            if (!crawler->children[index])
-                crawler->children[index] = getNode();
-
-            crawler = crawler->children[index];
-        }
-
-        crawler->isEndOfWord = true;
-    }
-
-    /**Returns if the word is in the trie. */
-    string search(string word)
-    {
-        trieNode *crawler = root;
-
-        for (int i = 0; i < word.length(); i++)
-        {
-            int index = word[i] - 'a';
-            if (!crawler->children[index])
-                return word;
-            crawler = crawler->children[index];
-            if (crawler->isEndOfWord)
+           	//try all length substring starting from 0th index
+            for (int l = 1; l <= word.length(); l++)
             {
-                return word.substr(0, i + 1);
+                string root = word.substr(0, l);
+                if (st.count(root))
+                {
+                    return root;
+                }
             }
+
+            return word;
         }
-        return word;
-    }
 
     string replaceWords(vector<string> &dictionary, string sentence)
     {
+        unordered_set<string> st(dictionary.begin(), dictionary.end());
 
         stringstream ss(sentence);
         string word;
         string result;
-        root = getNode();
-        for (string word: dictionary)
-        {
-            insert(word);
-        }
 
         while (getline(ss, word, ' '))
         {
-            result += search(word) + " ";
+            result += findRoot(word, st) + " ";
         }
 
         result.pop_back();
