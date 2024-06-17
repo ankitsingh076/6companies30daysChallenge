6a937a27f41e2b7167405a8e9924f77eb16e7cd1
Ankit Kumar Singh <ankitraj.1600405@gmail.com>
2024-06-18 00:24:40 +0530

    Time: 54 ms (5.4%), Space: 7.2 MB (18.45%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -5,11 +5,28 @@ class Solution
         {
             for (long a = 0; a * a <= c; a++)
             {
-                double b = sqrt(c - a *a);
 
-                if (b == (int) b)
+                int x = c - (int)(a *a);
+               	//b *b == x (we need to find the b which satisfies this)
+
+                int b_start = 0, b_end = x;
+
+                while (b_start <= b_end)
                 {
-                    return true;
+                    long mid = b_start + (b_end - b_start) / 2;
+
+                    if (mid *mid == x)
+                    {
+                        return true;
+                    }
+                    else if (mid * mid < x)
+                    {
+                        b_start = mid + 1;
+                    }
+                    else
+                    {
+                        b_end = mid - 1;
+                    }
                 }
             }
 
