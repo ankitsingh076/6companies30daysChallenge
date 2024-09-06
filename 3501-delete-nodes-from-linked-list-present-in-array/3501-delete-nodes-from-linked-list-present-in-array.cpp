4a4ef959d84ed071a92281fe73a884efe86587fd
Ankit Kumar Singh <ankitraj.1600405@gmail.com>
2024-09-06 22:44:10 +0530

    Time: 417 ms (83.8%), Space: 258.4 MB (91.24%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+        ListNode* modifiedList(vector<int> &nums, ListNode *head)
+        {
+            unordered_set<int> s;
+            int n = nums.size();
+            for (int i = 0; i < n; i++)
+            {
+                s.insert(nums[i]);
+            }
+            ListNode *ans = head;
+            while (ans != NULL && s.find(ans->val) != s.end())
+            {
+                ans = ans->next;	//Check for the head node excusively
+            }
+            head = ans;
+            while (head != NULL && head->next != NULL)
+            {
+                if (s.find(head->next->val) != s.end())
+                {
+                    head->next = head->next->next;
+                    continue;
+                }
+                head = head->next;
+            }
+            return ans;
+        }
+};
