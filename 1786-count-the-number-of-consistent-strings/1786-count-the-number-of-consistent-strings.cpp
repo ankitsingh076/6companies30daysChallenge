19fb0d28550eaa20fc89d0dbb207a58c9e600837
Ankit Kumar Singh <ankitraj.1600405@gmail.com>
2024-09-12 23:04:00 +0530

    Time: 41 ms (82.18%), Space: 33.9 MB (58.62%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
+        // Create a vector to mark which characters are allowed
+        vector<bool> isAllowed(26, false);
+
+        // Mark all characters in 'allowed' as true in the isAllowed vector
+        for (int i = 0; i < allowed.length(); i++) {
+            isAllowed[allowed[i] - 'a'] = true;
+        }
+
         int consistentCount = 0;
 
+        // Iterate through each word in the words array
         for (string& word : words) {
-            bool isWordConsistent = true;
+            bool isConsistent = true;
 
+            // Check each character of the current word
             for (int i = 0; i < word.length(); i++) {
-                char currentChar = word[i];
-                bool isCharAllowed = false;
-
-                for (int j = 0; j < allowed.length(); j++) {
-                    if (allowed[j] == currentChar) {
-                        isCharAllowed = true;
-                        break;  
-                    }
-                }
-
-                if (!isCharAllowed) {
-                    isWordConsistent = false;
-                    break; 
+                // If any character is not allowed, mark as inconsistent and
+                // break
+                if (!isAllowed[word[i] - 'a']) {
+                    isConsistent = false;
+                    break;
                 }
             }
 
-            
-            if (isWordConsistent) {
+            // If the word is consistent, increment the count
+            if (isConsistent) {
                 consistentCount++;
             }
         }
