089274cd085c0e74c54e676ac37895e85a4b4544
Ankit Kumar Singh <ankitraj.1600405@gmail.com>
2024-10-09 00:42:20 +0530

    Time: 115 ms (40.31%), Space: 35.2 MB (41.42%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -6,18 +6,16 @@ class Solution
             stack<char> st;
             for (auto &ch: s)
             {
-                if (!st.empty() && ch == ']' && st.top() == '[')
+                if (ch == '[')
+                {
+                    st.push(ch);
+                }
+                else if (!st.empty())
                 {
                     st.pop();
                 }
-                else
-                {
-                    if (ch == '[')
-                    {
-                        st.push(ch);
-                    }
-                }
             }
+            
             return (st.size() + 1) / 2;
         }
 };
