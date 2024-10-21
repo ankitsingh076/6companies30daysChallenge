793baadac4d30ca77c699659bbbadc212373cae5
Ankit Kumar Singh <ankitraj.1600405@gmail.com>
2024-10-22 00:10:11 +0530

    Time: 224 ms (60.82%), Space: 47.9 MB (66.67%) - LeetHub

---
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+        void solve(string &s, int index, int &max_count, int curr_count, unordered_set<string> &st)
+        {
+            if (index == s.length())
+            {
+                max_count = max(max_count, curr_count);
+            }
+
+            for (int j = index; j < s.length(); j++)
+            {
+                string str = s.substr(index, j - index + 1);
+                if (st.find(str) == st.end())
+                {
+                    st.insert(str);
+                    solve(s, j+1, max_count, curr_count + 1, st);
+                    st.erase(str);
+                }
+            }
+        }
+
+    int maxUniqueSplit(string s)
+    {
+        unordered_set<string> st;
+        int max_count = 0;
+        int curr_count = 0;
+        solve(s, 0, max_count, curr_count, st);
+
+        return max_count;
+    }
+};
