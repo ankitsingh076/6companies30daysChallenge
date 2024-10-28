ca6926ed09eae2aa5fee0d88df9bac0bc9672cf3
Ankit Kumar Singh <ankitraj.1600405@gmail.com>
2024-10-29 00:51:45 +0530

    Time: 99 ms (55.56%), Space: 108.9 MB (75.24%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -3,28 +3,28 @@ class Solution
     public:
         int longestSquareStreak(vector<int> &nums)
         {
-            unordered_set<int> st(begin(nums), end(nums));
+            unordered_map<int, int> mp;
+
+            sort(begin(nums), end(nums));
 
             int maxStreak = 0;
 
             for (int &num: nums)
             {
-                int streak = 0;
-                long long curr = num;
+            	//2, 4 
+               	//check if this number is a perfect square
+                int root = (int) sqrt(num);
 
-                while (st.find(curr) != st.end())
+                if (root *root == num && mp.find(root) != mp.end())
                 {
-                    streak++;
-
-                    if (curr * curr > 1e5)
-                    {
-                        break;
-                    }
-
-                    curr = curr * curr;	//square
+                    mp[num] = mp[root] + 1;
+                }
+                else
+                {
+                    mp[num] = 1;
                 }
 
-                maxStreak = max(maxStreak, streak);
+                maxStreak = max(maxStreak, mp[num]);
             }
 
             return maxStreak < 2 ? -1 : maxStreak;
